e927630d12dfd37411a532f7457749d8acc1795c
tommycmc <47530860+tommycmc@users.noreply.github.com>
2019-04-26 17:53:40 +0800

    new aggregate function and sorting function demo
    For demonstration: new aggregate function, i.e. SUM, AVER, MEDIAN, MIN, MAX; new sorting ability in sort function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <vector>
 #include "Record.h"
@@ -11,10 +12,10 @@ int main(){
     Database database(databaseSize);                                                                    //initialize the database
     
     
-    Record acc1{10000, "USD", "F", "C", "001123456789", "11042019", "2300"};                            //initialize a record - for demonstration only//
+    Record acc1{10000, "USD", "F", "C", "001123456789", "11042018", "2300"};                            //initialize a record - for demonstration only//
 
     for(int i{0}; i<50; i++){
-        Record acc{100000.0-i, "USD", "F", "C", "001123456789", "31032019", to_string(1259-i)};
+        Record acc{100000.0-i, "USD", "F", "C", "001123456789", "31032018", to_string(1259-i)};
         database.addRecord(acc, numOfRecord, databaseSize);                                             //add 50 records to database
     }                                                                                                   //initial size of database = 3
                                                                                                         //1st expansion of database = 6
@@ -34,6 +35,8 @@ int main(){
     
     cout << "\n" << numOfRecord << " records"<< endl;                                                   //display the no. of records in database
     database.showDatabase(numOfRecord);                                                                 //display all the records in database
+                                                                                                        //sort by amount in ascending order - default
+    
     
     cout << endl;
     database.changeDataInRecord(3, 700.9, "JPY", "T", "A", "NULL", "21022019", "1228");                 //change 4th record(pos = 3)
@@ -41,7 +44,10 @@ int main(){
     cout << endl;
     database.changeDataInRecord(6, 500.54, "USD", "A", "A", "NULL", "20022019", "1300");                //change 7th record(pos = 6)
 
+    cout << "\n" << numOfRecord << " records"<< endl;                                                   //display the no. of records in database
+    database.showDatabase(numOfRecord);                                                                 //display all the records in database
     
+
     cout << endl;
     database.filterShowDatabase(numOfRecord, 7);                                                        //filter the records in database with criteria(WHERE)
                                                                                                         //1:   Amount
@@ -56,15 +62,15 @@ int main(){
     cout << "\n" << numOfRecord << " records"<< endl;                                                   //display the no. of records in database
     database.showDatabase(numOfRecord);                                                                 //display all the records in database
 
-    int choice{1};                                                                                      //initialize the sort choice no.
-    database.sortDatabase(numOfRecord, choice);                                                         //sort the records with different priority(GROUP BY)
-                                                                                                        //1:    characterOfPayment                
-                                                                                                        //2:    typeOfPaymentCond
-                                                                                                        //3:    date + Time
-    
-    cout << "\n" << numOfRecord << " records"<< endl;                                                   //display the no. of records in database
-    database.showDatabase(numOfRecord);                                                                 //display all the records in database
-
+    database.sortDatabase(numOfRecord, 1);                                                              //sort the records with different priority(GROUP BY)
+                                                                                                        //1:    amount (ascending) - default
+                                                                                                        //2:    amount (descending)
+                                                                                                        //3:    characterOfPayment (ascending)
+                                                                                                        //4:    characterOfPayment (descending)
+                                                                                                        //5:    typeOfPaymentCond (ascending)
+                                                                                                        //6:    typeOfPaymentCond (descending)
+                                                                                                        //7:    date + Time (ascending)
+                                                                                                        //8:    date + Time (descending)
     
     cout << endl;
     database.deleteRecord(6, numOfRecord, databaseSize);                                                //delete 7th record(pos = 6)
@@ -73,7 +79,12 @@ int main(){
     database.showDatabase(numOfRecord);                                                                 //display all the records in database
     
 
-
+    cout << fixed << endl;;
+    cout << "Total: " << setw(6) << setprecision(2) << database.SUM(numOfRecord) << endl;
+    cout << "Mean: " << database.AVERAGE(numOfRecord) << endl;
+    cout << "Median: " << database.MEDIAN(numOfRecord) << endl;
+    cout << "Minimum: " << database.MIN(numOfRecord) << endl;
+    cout << "Maximum: " << database.MAX(numOfRecord) << endl;
     
     return 0;
 }
