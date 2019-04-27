d5412e3d983a127cb067fd292eed48bd6866c404
tommycmc <47530860+tommycmc@users.noreply.github.com>
2019-04-27 18:43:16 +0800

    Add files via upload
    merge external utility function to record function

---
+++ utilityFunction.h
@@ -0,0 +1,12 @@
+#ifndef UTILITYFUNCTION_H
+#define UTILITYFUNCTION_H
+
+#include "Record.h"
+
+std::string allToUpper(std::string str);
+std::string allToLower(std::string str);
+std::string dateExtract(std::string date);
+bool checkDate(std::string date);
+
+
+#endif // UTILITYFUNCTION_H
