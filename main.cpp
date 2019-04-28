8dd51653ff01e090d42d416fda19afc248f29f30
tommycmc <47530860+tommycmc@users.noreply.github.com>
2019-04-28 15:33:14 +0800

    Update main.cpp
    correct the augment of database driver program

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <cmath>
+#include <fstream>
 #include "Record.h"
 #include "Database.h"
 #include "user_interface.h"
@@ -14,7 +15,7 @@ int main(){
     int  numOfRecord{0}, databaseSize{3};                                                               //initialize the size of database, no. of records in database
     Database database(databaseSize);                                                                    //initialize the database
     
-    topMenu(database, numOfRecord, databaseSize);
+    topMenu(database, databaseSize, numOfRecord);
 
     
     return 0;
