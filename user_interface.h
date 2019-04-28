dd4347528e60d2a88cea656978ea91b66dd0f756
tommycmc <47530860+tommycmc@users.noreply.github.com>
2019-04-28 15:36:10 +0800

    Update user_interface.h
    split validation functions to subprogram to enhance program readability

--- user_interface.h
+++ user_interface.h
@@ -1,6 +1,7 @@
 #ifndef USER_INTERFACE_H
 #define USER_INTERFACE_H
 #include "Database.h"
+#include <fstream>
 
 //user interface//
 void topMenu(Database &database, int &databaseSize, int &numOfRecord);
@@ -8,10 +9,16 @@ void menuAddRecord(Database &database, int &databaseSize, int &numOfRecord);
 void menuDelRecord(Database &database, int &databaseSize, int &numOfRecord);
 void menuBatchDel(Database &database, int &databaseSize, int &numOfRecord);
 void menuEditRecord(Database &database, int &databaseSize, int &numOfRecord);
+void menuDisplayRecord(Database &database, int &databaseSize, int &numOfRecord);
 void menuSummaryReport(Database &database, int &databaseSize, int &numOfRecord);
 void menuExit(Database &database, int &databaseSize, int &numOfRecord);
 
-
+//validation//
+void amountCheck(Database &database, const int &numOfRecord, double &amount);
+void currencyCheck(Database &database, string &currency, double &amount);
+void characterOfPaymentCheck(Database &database, string &characterOfPayment);
+void typeOfPaymentCheck(Database &database, string &typeOfPayment);
+void accNoOfBankCheck(Database &database, string &accNoOfBank);
 
 //report interface//
 void summaryReportoutput(Database &database, const int &numOfRecord, const int &choice);
